8cfb8579d3c31c7e70b667bbe18692e24849952d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-04-10 08:10:51 +0200

    Remove some justfile and replace with bash scripts
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ snippets/python/check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHON_ENV_PATH="${SCRIPT_DIR}/venv"
+
+# Create a python env
+python -m venv "${PYTHON_ENV_PATH}" || virtualenv "${PYTHON_ENV_PATH}"
+
+# Enter in the python env
+. "${PYTHON_ENV_PATH}/bin/activate"
+
+# Install dependencies
+pip install -r "${SCRIPT_DIR}/requirements.txt"
+
+# Check
+PYRIGHT_PYTHON_FORCE_VERSION=latest pyright "${SCRIPT_DIR}/src"
